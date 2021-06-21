e5a3923e28fe952b6bb04b03c483ba9daabe1c35
Esra Mehmedova <Esramehmedova03@gmail.com>
2021-06-21 15:58:41 +0300

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
