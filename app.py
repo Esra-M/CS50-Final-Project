ef8eacc7c3439bf44f42d4b93b6222c38effa097
Esra Mehmedova <Esramehmedova03@gmail.com>
2021-07-07 16:50:36 +0300

    Make login and logout

--- app.py
+++ app.py
@@ -1,28 +1,88 @@
-from flask import Flask, redirect, request, render_template, g, url_for
+from flask import Flask, redirect, request, render_template, g, url_for, session
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_session import Session
+from tempfile import mkdtemp
 import sqlite3
 
 # Configure application
 app = Flask(__name__)
-
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # home page
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    # connect to the database
+    con = sqlite3.connect("memories.db")
+    cur = con.cursor()
+    
+    # if logged in display the name of the user and give option to log out
+    if session.get('user_id'):
+        logged = True
+
+        # get the users name
+        userID = session['user_id']
+        cur.execute("SELECT username FROM users WHERE userID = ?", (userID,))
+        username = " " + cur.fetchall()[0][0]
+        con.commit()
+
+        return render_template("index.html", username=username, logged=logged)
+    else:
+        # if not logged in don't display the users name and give options to log in and register
+        username=""
+        logged = False
+        return render_template("index.html", username=username, logged=logged)
+    
 
 # login
 @app.route("/login",  methods=["GET", "POST"])
 def login():
+
     if request.method == "POST":
+        
+        # connect to the database
+        con = sqlite3.connect("memories.db")
+        cur = con.cursor()
+
+        # get the users information from the login form
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # set an error message for checking valid input 
+        userError = ""
+
+        # query database for username
+        cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+        rows = cur.fetchall()
+        con.commit()
+
+        # ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0][2], password):
+            userError = "Invalid username or password"
+            return render_template("login.html", userError=userError, username=username, password=password)
+
+        # remember which user has logged in
+        session["user_id"] = rows[0][0]
+
         return redirect("/")
     else:
+
+        # don't let the user to go back to the login page if already loged in
+        if session.get('user_id'):
+            return redirect("/")
+
         return render_template("login.html")
 
 # register
 @app.route("/register",  methods=["GET", "POST"])
 def register():
+
     if request.method == "POST":
 
         # get the users information from the registration form
@@ -30,13 +90,13 @@ def register():
         password = request.form.get("password")
         confirmation = request.form.get("confirmPassword")
 
+        # connect to the database
+        con = sqlite3.connect("memories.db")
+        cur = con.cursor()
+
         # hash the password
         passwordHash = generate_password_hash(password)
 
-        # connect to the database
-        con = sqlite3.connect("memories.db")
-        cur = con.cursor() 
-        
         # set error variables for validation of the registration form
         nameError = None
         passError = None
@@ -44,7 +104,7 @@ def register():
         # Ensure username is unique
         cur.execute("SELECT Count(*) FROM users WHERE lower(username) = ?", (username.lower(),))
         numOfRows=cur.fetchone()
-        
+
         if numOfRows[0] != 0:
             nameError="Username is not available"
             return render_template("register.html", nameError=nameError, username=username, password=password, confirmation=confirmation)
@@ -57,18 +117,33 @@ def register():
                 # Update the database with the users information
                 cur.execute("INSERT INTO users(username, passwordHash) VALUES(?, ?)", (username, passwordHash))
                 con.commit()
-                con.close() 
+
+                # Remember which user has logged in
+                cur.execute("SELECT userID FROM users WHERE username = ?", (username,))
+                userID = cur.fetchall()
+                con.commit()
+                session["user_id"] = int(userID[0][0])
 
                 # redirect them to the home page
                 return redirect("/")
 
     else:
+
+        # don't let the user to go back to the register page if already loged in
+        if session.get('user_id'):
+            return redirect("/")
+
         return render_template("register.html")
 
 # logout
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect the users to the login form
+    return redirect("/")
 
 # add
 @app.route("/add")
