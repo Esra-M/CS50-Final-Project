c0a666f295b30ce42129d6afc4b920d53542ddd8
Esra-M <esramehmedova03@gmail.com>
2021-11-26 17:43:35 +0300

    redesign Add and myMemories

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 # home page
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -159,6 +167,7 @@ def add():
         logged = True
     else:
         logged = False
+        return redirect("/login")
 
     if request.method == "POST":
         
